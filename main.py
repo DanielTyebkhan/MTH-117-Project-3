1b5eded613809ace0ac6d213868b42776c7a3dd6
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-11-08 10:12:12 -0500

    Comments

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def has_alpha(pixel):
     return pixel[3] != 0
 
 
-def calculate_volume(image_path, rotation, board_height, board_length):
+def calculate_volume(image_path, rotation, board_thickness, board_length):
     image = Image.open(image_path).rotate(rotation, expand=True).convert('RGBA')
     all_pixels = image.getdata()
     partition = defaultdict(lambda: [sys.maxsize, -sys.maxsize+1])
 
-    # Find min and max height of board at each point
+    # Find min and max y coordinates of board at each point
     for x in range(image.width):
         for y in range(image.height):
             if has_alpha(all_pixels.getpixel((x, y))):
@@ -25,24 +25,28 @@ def calculate_volume(image_path, rotation, board_height, board_length):
     # find integral of surface of the board
     x_coords = sorted(partition.keys())
     pixel_ratio = board_length / (x_coords[-1] - x_coords[0]) 
-    print(pixel_ratio)
     xy_int = 0
     for x in x_coords:
         bot, top = partition[x]
         xy_int += (top-bot) * pixel_ratio**2
 
-    volume = xy_int * board_height
+    # multiply volume by
+    volume = xy_int * board_thickness
     return volume
 
 
 def main():
-#   path = '../res/board.png'
-#   rotation = 270
-#   board_height = 3
-#   board_length = 66
-    path, rotation, board_height, board_length = [int (i) for i in sys.argv[1:]]
-    print(f'Board volume is {calculate_volume(path, rotation, board_height, board_length) / 61.024} liters')
+    path = sys.argv[1]
+    rotation, board_thickness, board_length = [float(i) for i in sys.argv[2:]]
+    volume = calculate_volume(path, rotation, board_thickness, board_length)
+    # convert inches^3 to liters
+    liters = volume / 61.024
+    print('Board volume is {:.2f} liters'.format(liters))
 
 
 if __name__ == '__main__':
+    """
+    Call with 
+    `python3 main.py [image_path] [rotation (degrees)] [board_thickness (inches)] [board_length(inches)]`
+    """
     main()
